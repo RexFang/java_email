fcc5b8aac6ccbf719af245824153e745ec707bcc
RexFang <fangruitao.work@foxmail.com>
2017-03-27 23:40:54 +0800

    Update EmailUtilTest.java

--- src/test/EmailUtilTest.java
+++ src/test/EmailUtilTest.java
@@ -23,7 +23,7 @@ public class EmailUtilTest {
 			String content = "当地时间23日，俄罗斯总统普京在莫斯科国际贸易中心举行年度记者会。记者会持续了近4个小时，普京一共回答了来自俄罗斯各个地区及全世界记者的47个问题。自2001年起，普京都会在每年12月中下旬举行年度记者会，这是他的第12次记者会。";
 			List<File> fileList = new ArrayList<File>();
 			fileList.add(new File("C:/Users/Rex/Desktop/log4j.properties"));
-			EmailUtil.sendEmail(title, content, "rex_test@yeah.net", "2316056859@qq.com", "2363271365@qq.com", fileList);
+			EmailUtil.sendEmail(title, content, "rex_test@yeah.net", "123@qq.com", "456@qq.com", fileList);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
